7e36b0030c9e071248800749d6e7bc0f5c6b5e88
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2022-10-17 00:00:02 -0300

    fix: add vercel url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3000/api',
+  baseURL: process.env.VERCEL_URL ?? 'http://localhost:3000/api',
 });
