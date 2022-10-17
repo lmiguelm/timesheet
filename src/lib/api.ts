7a10d6edb0be3dccd2a06be93a9d3eeddcdc6d1c
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2022-10-17 00:23:55 -0300

    fix: teste

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_BASE_URL}/api` || 'http://localhost:3000/api',
+  baseURL:
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api` || 'http://localhost:3000/api',
 });
